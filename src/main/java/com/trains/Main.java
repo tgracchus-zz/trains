99fc089f8694f31b2db202fc74b7e962026948b2
Ulises Olivenza <ulises.olivenza@gmail.com>
2018-01-11 11:25:34 +0100

    Add intro

--- src/main/java/com/trains/Main.java
+++ src/main/java/com/trains/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Please, introduce your graph in one line and hit enter");
         Scanner in = new Scanner(System.in);
         List<String> results = Trains.newDefaultTrains().run(in.nextLine());
         results.forEach(System.out::println);
